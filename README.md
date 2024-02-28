db346bf8a78f0018ab610109bc955ea56a7f7342
Mohammed Altoobi <ialtoobi@Mohammeds-MacBook-Pro.local>
2024-02-29 01:26:59 +0400

    Bug fixes and performance improvements

--- README.md
+++ README.md
@@ -1,43 +1,235 @@
-## Thawani-Manager
+## Thawani Manager 
 
-A Laravel package for integrating Thawani payment gateway.
+Thawani Manager is a Laravel package designed to simplify interactions with the Thawani payment gateway API, enabling easy integration of Thawani payment solutions into your Laravel application.
 
-## How to install
-You can use composer:
+## Features
+- Create and manage checkout sessions
+- Handle customer information and payment methods
+- Generate payment URLs for redirecting users to the Thawani payment page
+- Process payments through Thawani's API
 
-```composer require Ialtoobi/Thawani-Manager```
+## Installation
+### 1. Require the Package
 
-or clone the class using GIT:
+Use Composer to add the package to your Laravel project:
 
-    git clone https://github.com/ialtoobi/Thawani-Manager.git
+``` bash 
+composer require Ialtoobi/Thawani-Manager
+```
+
+or clone the package using GIT:
+
+git clone https://github.com/ialtoobi/Thawani-Manager.git
 or Download the archive by [clicking here](https://github.com/ialtoobi/Thawani-Manager/archive/master.zip).
 
-## Basic Usage
-### In .env file Initialize these configuration to specify environment (Testing || Production):
-```php
+### 2. Publish Configuration 
 
-#Change mode based on environment 'test' || 'live' for production.
+Publish the package configuration to customize settings like (Mode, Secret Key, Publishable Key):
+```bash
+php artisan vendor:publish --tag=config
+```
+### 3. Configure Environment Variables
+
+To configure your application to work with Thawani Pay, you need to set the following variables in your **.env file**. These settings will specify whether your application is in testing or production mode, provide the necessary API keys for production environment.
+
+- THAWANI_MODE: Set this to either 'test' or 'live' based on your environment. This determines which set of API keys and endpoints the ThawaniManager will use.
+
+- THAWANI_LIVE_SECRET_KEY: This is your live secret key provided by Thawani Pay. It is used to authenticate server-side API requests to Thawani.
+
+- THAWANI_LIVE_PUBLISHABLE_KEY=: This is your live publishable key provided by Thawani Pay. It is used to authenticate client-side requests.
+
+```php
 THAWANI_MODE=test
 
-# Thawani Pay Configuration for Production Environment
 THAWANI_LIVE_SECRET_KEY=your_live_secret_key
+
 THAWANI_LIVE_PUBLISHABLE_KEY=your_live_publishable_key
-
 ```
 
-### Use ThawaniManager class :
+## Basic Usage
+### Creating a Checkout Session To Generate Payment URL
+To create a new checkout session and redirect the user to the Thawani payment page:
+
 ```php
-    use Ialtoobi\Thawani\ThawaniManager;
+use Ialtoobi\Thawani\ThawaniManager;
+use Illuminate\Support\Facades\Session;
 
-    $thawaniManager = new ThawaniManager();
+$thawani = new ThawaniManager();
+
+$data = [ 
+    "client_reference_id" => "123412",
+    "mode" => "payment",
+    "products" => [
+        [
+            "name" => "product 1",
+            "quantity" => 1,
+            "unit_amount" => 100,
+        ],
+    ],
+    "success_url" => "https://company.com/success",
+    "cancel_url" => "https://company.com/cancel",
+    "metadata" => [
+        "Customer name" => "ialtoobi",
+        "order id" => 0,
+    ],
+];
+
+$create_checkout_session_result = $thawani->createCheckoutSession($data);
+
+if ($create_checkout_session_result) {
+
+    $session_id = $create_checkout_session_result['session_id'];
+    $redirect_url = $create_checkout_session_result['redirect_url'];
+
+    // Save session_id for later use, such as confirming payment status
+    Session::put('session_id', $session_id);
+
+    return redirect($redirect_url);
+}else {
+    // Handle errors or unsuccessful attempts here, such as logging the error or notifying the user
+}
 ```
 
-### Create Checkout Session To Generate Payment URL:
+### Retrieving a Checkout Session
+To retrieve details of an existing checkout session use `session_id`:
 ```php
+// Assume $session_id is retrieved from a previous step or stored session
+$session_id = 'your_session_id_here';
 
+$retrieve_checkout_session_result  = $thawani->retrieveCheckoutSession($session_id);
+
+$payment_status = $retrieve_checkout_session_result['data']['status'];
+
+//Check payment status
+if ($payment_status === 'paid') {
+    // Handle successful payment
+    // This could include updating order status in your database
+}
 ```
 
-### Check Payment Status:
+### Managing Customers:
+
+Create a customer to save card for later payment:
+
+Make sure you provide created `customer_id` it in create checkout session.
 ```php
+// Use a unique string to reference your customer 
+$customer_data = [
+    "client_customer_id": "customer_email@mail.com"
+];
 
+$create_customer_result = $thawani->createCustomer($customer_data);
+
+//Save `customer_id` in the database to use for payment intent later
+$customer_id = $create_customer_result['data']['id'];
 ```
+
+To retrieve a customer use `customer_id`:
+```php
+$customer_id = 'your_customer_id_here';
+
+$retrieve_customer_result = $thawani->retrieveCustomer($customer_id);
+
+$data = $retrieve_customer_result['data'];
+```
+
+To delete a customer use `customer_id`:
+```php
+$customer_id = 'your_customer_id_here';
+
+$delete_customer_result = $thawani->deleteCustomer($customer_id);
+
+$description = $delete_customer_result['description'];
+```
+
+### Handling Payments:
+
+To get list of saved payment methods related to customer using `customer_id`:
+```php
+$customer_id = 'your_customer_id_here';
+
+$customer_payment_method_result = $thawani->customerPaymentMethod($customer_id);
+
+//Example: select first `payment_method_id` to create payment intent later
+$payment_method_id = $customer_payment_method_result['data'][0]['id'];
+```
+
+To delete a payment method use `card_id`:
+```php
+$card_id = 'card_id';
+
+$payment_method_result = $thawani->deletePaymentMethod($card_id);
+
+$description = $payment_method_result['description'];
+```
+
+### Payment Intent 
+To create a payment intent:
+```php
+$paymentData = [
+    "payment_method_id" => $payment_method_id, 
+    "amount" => 100,
+    "client_reference_id" => 1234,
+    "return_url" => "https://company.com/success",
+    "metadata" => [
+        "customer_name" => 'Mohammed Al-Toubi',
+        "order_id" => 1
+    ]
+];
+
+$payment_intent_id = $thawani->createPaymentIntent($paymentData); 
+```
+
+To confirm a payment intent:
+```php
+$payment_intent_id = 'your_payment_intent_id_here';
+
+$confirm_payment_intent_result = $thawani->confirmPaymentIntent($payment_intent_id);
+
+if ($confirm_payment_intent_result) {
+    
+    $payment_id = $confirm_payment_intent_result['payment_id'];
+    $redirect_url = $confirm_payment_intent_result['redirect_url'];
+
+    // Save payment_id for later use, such as confirming payment status
+    Session::put('payment_id', $payment_id);
+
+    return redirect($redirect_url);
+}else {
+    // Handle errors or unsuccessful attempts here, such as logging the error or notifying the user
+}
+```
+To retrieve details of payment intent use `payment_intent_id`:
+```php
+// Assume $payment_id is retrieved from a previous step or stored session
+$payment_id = 'your_payment_id_here';
+
+$retrieve_payment_intent_result  = $thawani->retrievePaymentIntent($payment_id);
+
+$payment_status = $retrieve_payment_intent_result['data']['status'];
+
+//Check payment status
+if ($payment_status === 'succeeded') {
+    // Handle successful payment
+    // This could include updating order status in your database
+}
+```
+
+To cancel paymant intent use `payment_intent_id`:
+```php
+$payment_intent_id = 'your_payment_intent_id_here';
+
+$cancel_payment_intent_result = $thawani->cancelPaymentIntent($payment_intent_id);
+
+$description = $cancel_payment_intent_result['description'];
+```
+
+## Support
+For issues, questions, or contributions, please open an issue on the GitHub repository: [clicking here](https://github.com/ialtoobi/Thawani-Manager)
+
+#### Follow me on X [@ialtoobi](https://x.com/ialtoobi/)
+
+## License
+Thawani Manager is open-sourced package licensed under the MIT license.
+
+
--- src/ThawaniManager.php
+++ src/ThawaniManager.php
@@ -7,52 +7,169 @@ use Illuminate\Support\Facades\Http;
 
 class ThawaniManager
 {
+    /**
+     * Create a checkout session. After the session has been created,
+     * use the session_id to redirect the user to the Thawani payment hosted page.
+     *
+     * url format: https://uatcheckout.thawani.om/pay/{session_id}?key=publishable_key
+     *
+     * @param array $data
+     * @return array
+     */
     public function createCheckoutSession(array $data)
     {
         $response = $this->makeRequest('post', '/checkout/session', $data);
 
         if (isset($response['success']) && $response['success'] === true && $response['code'] === 2004) {
-            return $response['data']['session_id'];
+
+            //return $response['data']['session_id'];
+            $session_id = $response['data']['session_id'];
+            $redirect_url = $this->createCheckoutURL($session_id);
+
+            return [
+                'session_id' => $session_id,
+                'redirect_url' => $redirect_url,
+            ];
         }
 
         $this->handleError($response);
     }
 
-    public function getCheckoutSession(string $sessionId)
+    /**
+     * Get checkout session by session_id.
+     *
+     * https://uatcheckout.thawani.om/api/v1/checkout/session/{session_id}
+     *
+     * @param string $session_id
+     * @return array
+     */
+    public function retrieveCheckoutSession(string $session_id)
     {
-        return $this->makeRequest('get', "/checkout/session/{$sessionId}");
+        return $this->makeRequest('get', "/checkout/session/{$session_id}");
     }
 
+    /**
+     * Get a list of sessions.
+     *
+     * https://uatcheckout.thawani.om/api/v1/checkout/session?limit=10&skip=20
+     *
+     * @param string limit & skip
+     * @return array
+     */
     public function getAllCheckoutSession()
     {
         return $this->makeRequest('get', "/checkout/session/");
     }
 
-    public function cancelCheckoutSession(string $sessionId)
+    /**
+     * Cancel Checkout Session, by providing session_id.
+     *
+     * https://uatcheckout.thawani.om/api/v1/checkout/{session_id}/cancel
+     *
+     * @param string $session_id
+     * @return array
+     */
+    public function cancelCheckoutSession(string $session_id)
     {
-        return $this->makeRequest('post', "/checkout/{$sessionId}/cancel");
+        return $this->makeRequest('post', "/checkout/{$session_id}/cancel");
     }
 
+    /**
+     * Create customers to allow them to save cards for later payment
+     * since some Thawani services mandate the use of customer_id.
+     *
+     * https://uatcheckout.thawani.om/api/v1/customers
+     *
+     * @param array $data
+     * @return array
+     */
     public function createCustomer(array $data)
     {
         return $this->makeRequest('post', '/customers', $data);
     }
 
-    public function retrieveCustomer(string $customerId)
+    /**
+     * Get Customer by customer_id.
+     *
+     * https://uatcheckout.thawani.om/api/v1/customers/{customer_id}
+     *
+     * @param string $customer_id
+     * @return array
+     */
+    public function retrieveCustomer(string $customer_id)
     {
-        return $this->makeRequest('get', "/customers/{$customerId}/");
+        return $this->makeRequest('get', "/customers/{$customer_id}/");
     }
 
-    public function customerPaymentMethod(string $customerId)
+    /**
+     * Remove customer by customer_id.
+     *
+     * https://uatcheckout.thawani.om/api/v1/customers/{customer_id}
+     *
+     * @param string $customer_id
+     * @return array
+     */
+    public function deleteCustomer(string $customer_id)
     {
-        return $this->makeRequest('get', "/payment_methods?customer_id={$customerId}");
+        return $this->makeRequest('delete', "/customers/{$customer_id}/");
     }
 
-    public function deletePaymentMethod(string $cardId)
+    /**
+     * List Customer's Payment Method.
+     * Get payment methods related to customer using customer_id
+     *
+     * https://uatcheckout.thawani.om/api/v1/payment_methods
+     *
+     * @param string $customer_id
+     * @return array
+     */
+    public function customerPaymentMethod(string $customer_id)
     {
-        return $this->makeRequest('delete', "/payment_methods/{$cardId}");
+        // Get the base URL and secret key based on the environment
+        $baseUrl = self::getBaseUrl();
+        $secretKey = self::getSecretKey();
+
+        // Make the GET request to fetch the session
+        $response = Http::baseUrl($baseUrl)
+            ->withHeaders(['thawani-api-key' => $secretKey])
+            ->asJson()
+            ->get("/payment_methods?customer_id={$customer_id}");
+
+        // Check if the request was successful
+        if ($response->successful()) {
+            return $response->json();
+        }
+
+        // If not successful, throw an exception with the error description and code
+        $this->handleError($response);
+
+        //return $this->makeRequest('get', "/payment_methods?customer_id={$customer_id}");
     }
 
+    /**
+     * Remove payment method by card_id.
+     *
+     * https://uatcheckout.thawani.om/api/v1/payment_methods/{card_id}
+     *
+     * @param string $card_id
+     * @return array
+     */
+    public function deletePaymentMethod(string $card_id)
+    {
+        return $this->makeRequest('delete', "/payment_methods/{$card_id}");
+    }
+
+    /**
+     * Create Payment Intent. Payment intent used once the card of your customer saved,
+     * and you want to charge your customer off-session.
+     * Once Payment intent has been created use the payment_intent.id
+     * to confirm the payment intent and proceed with processing the payment.
+     *
+     * https://uatcheckout.thawani.om/api/v1/payment_intents
+     *
+     * @param array $data
+     * @return array
+     */
     public function createPaymentIntent(array $data)
     {
         $response = $this->makeRequest('post', '/payment_intents', $data);
@@ -64,21 +181,75 @@ class ThawaniManager
         $this->handleError($response);
     }
 
-    public function confirmPaymentIntent(string $paymentIntentId)
+    /**
+     * Payment Intent confirmation. payment_method_id should be provided here
+     * if it didn't happen at creating Payment Intent.
+     *
+     * https://uatcheckout.thawani.om/api/v1/payment_intents/{payment_intent_id}/confirm
+     *
+     * @param string $payment_intent_id
+     * @return array
+     */
+    public function confirmPaymentIntent(string $payment_intent_id)
     {
-        return $this->makeRequest('post', "/payment_intents/{$paymentIntentId}/confirm");
+        $response = $this->makeRequest('post', "/payment_intents/{$payment_intent_id}/confirm");
+
+        if (isset($response['success']) && $response['success'] === true && $response['code'] === 2000) {
+
+            $payment_id = $response['data']['id'];
+            $redirect_url = $response['data']['next_action']['url'];
+
+            return [
+                'payment_id' => $payment_id,
+                'redirect_url' => $redirect_url,
+            ];
+        }
+
+        $this->handleError($response);
+
+        //return $this->makeRequest('post', "/payment_intents/{$payment_intent_id}/confirm");
     }
 
-    public function retrievePaymentIntent(string $paymentIntentId)
+    /**
+     * Get Payment Intent by payment_intent_id.
+     *
+     * https://uatcheckout.thawani.om/api/v1/payment_intents/{payment_intent_id}
+     *
+     * @param string $payment_intent_id
+     * @return array
+     */
+    public function retrievePaymentIntent(string $payment_intent_id)
     {
-        return $this->makeRequest('get', "/payment_intents/{$paymentIntentId}");
+        return $this->makeRequest('get', "/payment_intents/{$payment_intent_id}");
     }
 
-    public function cancelPaymentIntent(string $paymentIntentId)
+    /**
+     * Cancel Payment Intent, by providing payment_intent_id.
+     *
+     * https://uatcheckout.thawani.om/api/v1/payment_intents/{payment_intent_id}/cancel
+     *
+     * @param string $payment_intent_id
+     * @return array
+     */
+    public function cancelPaymentIntent(string $payment_intent_id)
     {
-        return $this->makeRequest('post', "/payment_intents/{$paymentIntentId}/cancel");
+        return $this->makeRequest('post', "/payment_intents/{$payment_intent_id}/cancel");
     }
 
+    /**
+     * Performs an HTTP request to the Thawani API.
+     *
+     * This method constructs and executes an HTTP request using the specified method,
+     * URI, and data payload. It automatically includes necessary headers such as the
+     * Thawani API key. On successful response, it returns the JSON-decoded content.
+     * If the request fails, it delegates error handling to `handleError`.
+     *
+     * @param  string $method The HTTP method (e.g., 'get', 'post').
+     * @param  string $uri The URI to request, relative to the base API URL.
+     * @param  array $data The data to send with the request (for POST, PUT requests).
+     * @return array The JSON-decoded response data.
+     * @throws Exception if the request fails or the API returns an error.
+     */
     protected function makeRequest(string $method, string $uri, array $data = [])
     {
         $baseUrl = $this->getBaseUrl();
@@ -93,27 +264,113 @@ class ThawaniManager
             return $response->json();
         }
 
+        // if ($response->failed()) {
+        //     return [
+        //         'url' => $baseUrl . $uri,
+        //         'response' => $response->json(),
+        //         'status' => $response->status(),
+        //     ];
+        //     throw new Exception("API Request Failed: " . $response->body());
+        // }
+
         $this->handleError($response->json());
     }
 
+    /**
+     * Handles errors returned by the Thawani API.
+     *
+     * This method is invoked when `makeRequest` encounters an unsuccessful response.
+     * It throws an exception with the error description and code provided by the API.
+     * If no specific error information is available, it throws a generic error message.
+     *
+     * @param array $response The JSON-decoded response array from the API.
+     * @throws Exception with the error message and code.
+     */
     protected function handleError($response)
     {
         throw new Exception($response['description'] ?? 'Unknown error', $response['code'] ?? 0);
     }
 
+    /**
+     * Retrieves Thawani API configuration settings.
+     *
+     * This method fetches the configuration settings for the Thawani API based on the
+     * current environment mode (e.g., 'test', 'live'). It returns configuration details
+     * such as the base URL and secret key.
+     *
+     * The configuration settings for the current mode.
+     */
     protected function getThawaniConfig()
     {
         $mode = config('thawani.mode');
         return config("thawani.{$mode}");
     }
 
+    /**
+     * Retrieves the base URL for Thawani API requests.
+     *
+     * This method extracts and returns the base URL from the Thawani configuration
+     * settings. It determines the correct base URL based on the current environment mode.
+     *
+     * @return string The base URL for the Thawani API.
+     */
     protected function getBaseUrl(): string
     {
         return $this->getThawaniConfig()['base_url'];
     }
 
+
+    /**
+     * Retrieves the secret key for authenticating Thawani API requests.
+     *
+     * @return string The secret key for the Thawani API.
+     */
     protected function getSecretKey(): string
     {
-        return $this->getThawaniConfig()['secret_key'];
+        $config = $this->getThawaniConfig();
+        return $config['secret_key'];
+    }
+
+    /**
+     * Retrieves the publishable key required for redirecting to the Thawani payment page.
+     *
+     * https://uatcheckout.thawani.om/pay/{session_id}?key=publishable_key
+     *
+     * @return string The publishable key for the Thawani API, necessary for payment page redirection.
+     * @throws Exception If the publishable key is not found in the configuration, indicating that the Thawani API integration may not be correctly set up for the current operational mode ('test' or 'live').
+     */
+    protected function getPublishableKey(): string
+    {
+        $config = $this->getThawaniConfig();
+
+        if (!isset($config['publishable_key'])) {
+            throw new Exception("Publishable key not configured for Thawani '{$config['mode']}' mode.");
+        }
+        return $config['publishable_key'];
+    }
+
+    /**
+     * Generates the URL for redirecting to the Thawani payment page.
+     *
+     * This method constructs the URL required to redirect users to the Thawani payment page,
+     * incorporating the session ID and the publishable key into the URL. The session ID is
+     * used to identify the specific payment session, and the publishable key authenticates
+     * the request client-side without exposing sensitive information.
+     *
+     * @param string $session_id The unique identifier for the payment session.
+     * @return string The fully constructed URL for initiating the payment process on Thawani.
+     */
+    public function createCheckoutURL($session_id): string
+    {
+        // Retrieve the publishable key from the configuration.
+        $publishableKey = $this->getPublishableKey();
+
+        // Retrieve the checkout base URL from the configuration (thawani.php).
+        $checkoutBaseUrl = $this->getThawaniConfig()['checkout_base_url'];
+
+        // Construct and return the full URL for redirecting to the Thawani payment page.
+        $redirectUrl = "{$checkoutBaseUrl}/{$session_id}?key={$publishableKey}";
+
+        return $redirectUrl;
     }
 }
